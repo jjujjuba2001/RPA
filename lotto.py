4924accee847fa171b27d1c744fac78080704d15
jjujjuba2001 <jjujjuba2001@naver.com>
2024-07-15 15:23:33 +0900

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -7,20 +7,20 @@ print ('=' * 50)
 
 for i in range(7):
     num = random.randrange(1,46)
-    while num in result:
+    while num in result:                   # 중복 숫자 제거하기, 리스트 안에 num 이 있다면 다시 뽑기
         num = random.randrange(1,46)
     result.append(num)
 
 
 for j in range(7):
     if result[j] == result[5]:
-        print (result[j], end=' ')
+        print (result[j], end=' ')             # 보너스 번호 전에는 공백 출력하기
         
     elif result[j] == result[6]:
-        print ("보너스 번호 : ", result[j])
+        print ("보너스 번호 : ", result[j])    # 보너스 번호 구분해 출력하기
         break
     else:    
-        print (result[j], end= ', ')
+        print (result[j], end= ', ')         # 가로 한줄 , 로 구분해 출력하기
 
 print ('=' * 50)
 
